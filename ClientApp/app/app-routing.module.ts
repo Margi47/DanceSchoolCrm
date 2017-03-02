02f6b1fd7872b9aebb809582c575dd76af05935f
Margarita <xgj@cgh>
2017-03-02 22:12:19 +0100

    detail component routing

--- ClientApp/app/app-routing.module.ts
+++ ClientApp/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 ﻿import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { UsersComponent } from './components/app/users.component'
 import { UserDetailComponent } from './components/app/user-detail.component';
 
 const routes: Routes = [
+    { path: '', redirectTo: '/userlist', pathMatch: 'full' },
+    { path: 'userlist', component: UsersComponent },
     { path: 'detail', component: UserDetailComponent }
 ];
 
