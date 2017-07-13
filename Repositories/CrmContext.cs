a8e8f261d00992b226a40ec836bf2b0c415ff34e
Margi47 <Margi47@users.noreply.github.com>
2017-07-13 14:02:16 +0200

    changed deleting pattern

--- Repositories/CrmContext.cs
+++ Repositories/CrmContext.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace angular.Models
@@ -10,7 +11,7 @@ namespace angular.Models
     {
         public CrmContext(DbContextOptions<CrmContext> options)
             : base(options)
-        {}
+        { }
 
         public DbSet<User> Users { get; set; }
         public DbSet<Group> Groups { get; set; }
@@ -50,6 +51,114 @@ namespace angular.Models
                 .HasOne(t => t.User)
                 .WithOne(i => i.TeacherInfo)
                 .HasForeignKey<Teacher>(t => t.Id);
+
+            modelBuilder.Entity<User>()
+                .Property<bool>("IsDeleted");
+
+            //modelBuilder.Entity<User>()
+              //  .HasQueryFilter(post => EF.Property<bool>(post, "IsDeleted") == false);
+
+            modelBuilder.Entity<Group>()
+                .Property<bool>("IsDeleted");
+
+            modelBuilder.Entity<Teacher>()
+                .Property<bool>("IsDeleted");
+
+            modelBuilder.Entity<GroupUser>()
+                .Property<bool>("IsDeleted");
+
+            modelBuilder.Entity<GroupTeachers>()
+                .Property<bool>("IsDeleted");
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            OnBeforeSaving();
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            OnBeforeSaving();
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        private void OnBeforeSaving()
+        {
+            foreach (var entry in ChangeTracker.Entries<User>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.CurrentValues["IsDeleted"] = false;
+                        break;
+
+                    case EntityState.Deleted:
+                        entry.State = EntityState.Modified;
+                        entry.CurrentValues["IsDeleted"] = true;
+                        break;
+                }
+            }
+
+            foreach (var entry in ChangeTracker.Entries<Group>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.CurrentValues["IsDeleted"] = false;
+                        break;
+
+                    case EntityState.Deleted:
+                        entry.State = EntityState.Modified;
+                        entry.CurrentValues["IsDeleted"] = true;
+                        break;
+                }
+            }
+
+            foreach (var entry in ChangeTracker.Entries<Teacher>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.CurrentValues["IsDeleted"] = false;
+                        break;
+
+                    case EntityState.Deleted:
+                        entry.State = EntityState.Modified;
+                        entry.CurrentValues["IsDeleted"] = true;
+                        break;
+                }
+            }
+
+            foreach (var entry in ChangeTracker.Entries<GroupUser>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.CurrentValues["IsDeleted"] = false;
+                        break;
+
+                    case EntityState.Deleted:
+                        entry.State = EntityState.Modified;
+                        entry.CurrentValues["IsDeleted"] = true;
+                        break;
+                }
+            }
+
+            foreach (var entry in ChangeTracker.Entries<GroupTeachers>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.CurrentValues["IsDeleted"] = false;
+                        break;
+
+                    case EntityState.Deleted:
+                        entry.State = EntityState.Modified;
+                        entry.CurrentValues["IsDeleted"] = true;
+                        break;
+                }
+            }
         }
     }
 }
