d01d4edac6190a91e5b76690e452ad3f3f2fc0ac
Margi47 <Margi47@users.noreply.github.com>
2017-09-22 18:09:34 +0200

    logger configurations added

--- crm-webapi/Startup.cs
+++ crm-webapi/Startup.cs
@@ -21,69 +21,70 @@ using System.IO;
 
 namespace crm_webapi
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
+      Configuration = configuration;
+
+
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      var connectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=Crm;Integrated Security=True;MultipleActiveResultSets=True";
+      services.AddDbContext<CrmContext>(options =>
+          options.UseSqlServer(connectionString));
+
+      services.AddMvc(
+          config =>
+          {
+            config.Filters.Add(typeof(HandleExceptionAttribute));
+            config.Filters.Add(typeof(ApiValidationAttribute));
+          });
 
       Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Debug()
-             .WriteTo.Console()
-             .WriteTo.RollingFile("log-{Date}.txt")
-            .CreateLogger();
+      .MinimumLevel.Debug()
+       .WriteTo.Console()
+       .WriteTo.RollingFile("log-{Date}.txt")
+      .CreateLogger();
+
+      services.AddScoped<IUserRepository, UserRepository>();
+      services.AddScoped<IGroupRepository, GroupRepository>();
+      services.AddScoped<ITeacherRepository, TeacherRepository>();
+      services.AddScoped<IGroupUserRepository, GroupUserRepository>();
+      services.AddScoped<IGroupTeacherRepository, GroupTeacherRepository>();
+      services.AddCors();
+
+      Mapper.Initialize(cfg =>
+      {
+        cfg.CreateMap<User, UserApiModel>();
+        cfg.CreateMap<UserApiModel, User>();
+        cfg.CreateMap<Group, GroupApiModel>();
+        cfg.CreateMap<GroupApiModel, Group>();
+        cfg.CreateMap<Teacher, TeacherApiModel>();
+        cfg.CreateMap<TeacherApiModel, Teacher>();
+        cfg.CreateMap<User, TeacherApiModel>();
+        cfg.CreateMap<TeacherApiModel, User>();
+        cfg.CreateMap<Teacher, TeacherApiModel>()
+              .ForMember(x => x.Id, y => y.MapFrom(src => src.Id))
+              .ForMember(x => x.Name, y => y.MapFrom(src => src.User.Name));
+      });
     }
 
-        public IConfiguration Configuration { get; }
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            var connectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=Crm;Integrated Security=True;MultipleActiveResultSets=True";
-            services.AddDbContext<CrmContext>(options =>
-                options.UseSqlServer(connectionString));
-
-            services.AddMvc(
-                config =>
-                {
-                    config.Filters.Add(typeof(HandleExceptionAttribute));
-                    config.Filters.Add(typeof(ApiValidationAttribute));
-                });
-
-            services.AddScoped<IUserRepository, UserRepository>();
-            services.AddScoped<IGroupRepository, GroupRepository>();
-            services.AddScoped<ITeacherRepository, TeacherRepository>();
-            services.AddScoped<IGroupUserRepository, GroupUserRepository>();
-            services.AddScoped<IGroupTeacherRepository, GroupTeacherRepository>();
-            services.AddCors();
-
-            Mapper.Initialize(cfg =>
-            {
-                cfg.CreateMap<User, UserApiModel>();
-                cfg.CreateMap<UserApiModel, User>();
-                cfg.CreateMap<Group, GroupApiModel>();
-                cfg.CreateMap<GroupApiModel, Group>();
-                cfg.CreateMap<Teacher, TeacherApiModel>();
-                cfg.CreateMap<TeacherApiModel, Teacher>();
-                cfg.CreateMap<User, TeacherApiModel>();
-                cfg.CreateMap<TeacherApiModel, User>();
-                cfg.CreateMap<Teacher, TeacherApiModel>()
-                .ForMember(x => x.Id, y => y.MapFrom(src => src.Id))
-                .ForMember(x => x.Name, y => y.MapFrom(src => src.User.Name));
-            });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-            app.UseMvc();
-
-        }
+      app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+      app.UseMvc();
     }
+  }
 }
